12d20ab4598a6cef63cc964d8523100958bb9028
AndrewSiwi <67230305+AndrewSiwi@users.noreply.github.com>
2023-08-12 12:20:01 +0200

    update INTERPRETER

--- INTERPRETER/INTERPRETER(10033).cpp
+++ INTERPRETER/INTERPRETER(10033).cpp
@@ -10,39 +10,68 @@ using namespace std;
 
 #define ITER_CONT(cont) for(auto& x: cont)
 
-#define N 10
+#define N 1000
 
 class Solution
 {
     static int t;
     public: static void init()
     {
-        //cin >> t;
+        cin >> t;
+        cin.ignore();
+        cin.ignore();
 
         ITER_ARR(0, i, t)
         {
-            Solution* sl = new Solution();
+            Solution* sl = new Solution(i);
             sl->process();
             sl->print();
             delete sl;
         }
     }
 
+    private:
+
+    int ct;
+
+    string RAM[N] = { "000" };
+    int regs[10] = { 0 };
+    int ip = 0;
+    int ret = 0;
+
 
     public:
 
-    Solution()
+    Solution(int ct)
     {
+        this->ct = ct;
 
+        string s;
+        int ip = 0;
+        while(true)
+        {
+            if(!getline(cin, s) || s.size() == 0) break;
+            this->RAM[ip++] = s;
+        }
     }
 
     void process()
     {
+        while(this->RAM[this->ip] != "100")
+        {
+            string cmd = this->RAM[this->ip++];
+            char c = cmd[0]; int d = cmd[1] - '0', n = cmd[2] - '0';
+            if(c = '2') this->regs[d] = n;
 
+            this->ret++;
+        }
+        this->ret++;
     }
 
     void print()
     {
+        if(this->ct > 0) cout << "\n";
+        cout << this->ret << "\n";
         //cout << "\n";
     }
 };
